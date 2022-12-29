8729cbdce248e7ae31e71c7904c67dc00e55782c
Micah Lindley <micahincorporated@gmail.com>
2022-12-29 15:32:03 -0600

    Initial working production version

--- public/converter.js
+++ public/converter.js
@@ -1,16 +1,27 @@
 const path = require("path");
 const ffmpeg = require("ffmpeg-static");
 const { spawn } = require("child_process");
+const dev = require("electron-is-dev");
 
 module.exports = (filePath, outputFolder, format, messager, callback) => {
   const output =
     path.join(outputFolder, path.basename(filePath, path.extname(filePath))) +
     "-converted." +
     format;
-  console.log(`OUTPUT ${output}`);
-  console.log(`CONVERTING FILE "${filePath}" TO FORMAT ${format}`);
-  console.log(`USING FFMPEG @ ${ffmpeg}`);
-  const command = spawn(`${ffmpeg}`, ["-i", `${filePath}`, `${output}`]);
+  messager.send("stdout", ffmpeg.toString());
+  let command;
+  if (dev) {
+    command = spawn(`${ffmpeg}`, ["-i", `${filePath}`, `${output}`]);
+  } else {
+    console.log(`OUTPUT ${output}`);
+    console.log(`CONVERTING FILE "${filePath}" TO FORMAT ${format}`);
+    console.log(`USING FFMPEG @ ${ffmpeg}`);
+    command = spawn(`${ffmpeg.replace("app.asar", "app.asar.unpacked")}`, [
+      "-i",
+      `${filePath}`,
+      `${output}`,
+    ]);
+  }
   command.stdout.on("data", (d) => {
     console.log(d.toString());
     messager.send("stdout", d.toString());
